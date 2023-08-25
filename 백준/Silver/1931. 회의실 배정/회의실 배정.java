6de5b546d69e2193ab13db42c5e8e79673736e18
KimJungWoo <86278689+tiktakz@users.noreply.github.com>
2023-08-25 19:07:52 +0900

    [Silver I] Title: 회의실 배정, Time: 612 ms, Memory: 43204 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Meeting implements Comparable<Meeting> {
+        int start, end;
+        public Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Meeting o) {
+            if (this.end == o.end) {
+                return this.start - o.start;
+            }
+            else return this.end - o.end;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ArrayList<Meeting> meetings = new ArrayList<>();
+        int n = Integer.parseInt(st.nextToken());
+        for (int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            meetings.add(new Meeting(start, end));
+        }
+        Collections.sort(meetings);
+
+        int count = 0;
+        int endtime = 0;
+
+        for (int i=0; i<meetings.size(); i++) {
+            if (meetings.get(i).start >= endtime) {
+                count++;
+                endtime = meetings.get(i).end;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
