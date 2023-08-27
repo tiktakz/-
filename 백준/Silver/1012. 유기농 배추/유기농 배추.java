61c311b7e17c56098abf9a4ce18631b8821eade9
KimJungWoo <86278689+tiktakz@users.noreply.github.com>
2023-08-27 23:48:47 +0900

    [Silver II] Title: 유기농 배추, Time: 112 ms, Memory: 13620 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,55 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.*;
-
-class Main {
-    // t: testcase, m:가로, n:세로, k:배추개수
-    static int t, m, n, k, nx, ny;
-    static boolean[][] visited;
+public class Main {
+    static int testcase, m, n, k, worms;
     static int[][] graph;
-    static int dx[] = {1,-1,0,0};
-    static int dy[] = {0,0,-1,1};
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
+    static boolean[][] visited;
 
-    public static void DFS(int x, int y) {
-//        if (x<0 || y<0 || x>m || y>n) return; // 범위 밖
-        visited[x][y] = true;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i=0; i<4; i++) {
-            nx = x + dx[i];
-            ny = y + dy[i];
-            if (nx>=0 && ny>=0 && nx<m && ny<n) {
-                if (!visited[nx][ny] && graph[nx][ny] == 1) {
-                    DFS(nx, ny);
-                }
-            }
-        }
-    }
+        testcase = Integer.parseInt(st.nextToken());
 
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        t = sc.nextInt(); // Testcase numbers
-        for (int i=0; i<t; i++) {
-            m = sc.nextInt(); // 가로
-            n = sc.nextInt(); // 세로
-            k = sc.nextInt(); // 배추
+        for (int i=0; i<testcase; i++) {
+            st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
             graph = new int[m][n];
             visited = new boolean[m][n];
+
             for (int j=0; j<k; j++) {
-                int a = sc.nextInt();
-                int b = sc.nextInt();
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
                 graph[a][b] = 1;
             }
-            int count = 0;
-            for (int p=0; p<m; p++) {
-                for (int l=0; l<n; l++) {
-                    if (graph[p][l] == 1 && !visited[p][l]) {
-                        DFS(p,l);
-                        count++;
+
+            worms = 0;
+            for (int x=0; x<m; x++) {
+                for (int y=0; y<n; y++) {
+                    if (!visited[x][y] && graph[x][y] == 1) {
+                        worms++;
+                        DFS(x,y);
                     }
                 }
             }
+            System.out.println(worms);
+        }
+    }
 
-            System.out.println(count);
+    public static void DFS(int x, int y) {
+        visited[x][y] = true;
+        for (int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+                if (!visited[nx][ny] && graph[nx][ny] == 1) {
+                    DFS(nx,ny);
+                }
+            }
         }
     }
 }
