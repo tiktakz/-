38c07b59c72fd1007cda6c48272954bf2d6c19ca
KimJungWoo <86278689+tiktakz@users.noreply.github.com>
2023-09-01 19:25:01 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 92 ms, Memory: 13952 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,56 +1,68 @@
-import java.util.*;
 
-class Main {
-    static int[] map;
-    static int[] operations = new int[4];
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
     static int n;
-    static int MIN = Integer.MAX_VALUE;
-    static int MAX = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    static int max = Integer.MIN_VALUE;
+    static int[] arr;
+    static int[] operations;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        map = new int[n];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        operations = new int[4];
 
+        st = new StringTokenizer(br.readLine());
         for (int i=0; i<n; i++) {
-            map[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        st = new StringTokenizer(br.readLine());
         for (int i=0; i<4; i++) {
-            operations[i] = sc.nextInt();
+            // 순서 + - * /
+            operations[i] = Integer.parseInt(st.nextToken());
         }
 
-        DFS(map[0], 1);
-        System.out.println(MAX);
-        System.out.println(MIN);
+        // 첫번째 숫자 부터 시작
+        DFS(arr[0], 1);
+        System.out.println(max);
+        System.out.println(min);
     }
 
-    public static void DFS(int number, int depth) {
+    public static void DFS(int start, int depth) {
         if (depth == n) {
-            MAX = Math.max(MAX, number);
-            MIN = Math.min(MIN, number);
-            return;
+            // 깊이가 n은 모든 숫자를 다 돌았다는 뜻
+            min = Math.min(min, start);
+            max = Math.max(max, start);
         }
 
         for (int i=0; i<4; i++) {
+            // 해당 연산 부호가 남아있으면
             if (operations[i] > 0) {
-                operations[i]--;
-                switch (i) {
+                operations[i]--; // 연산 하나 처리
+                switch(i) {
                     case 0:
-                        DFS(number + map[depth], depth + 1);
+                        DFS(start + arr[depth], depth+1);
                         break;
                     case 1:
-                        DFS(number - map[depth], depth + 1);
+                        DFS(start - arr[depth], depth+1);
                         break;
                     case 2:
-                        DFS(number * map[depth], depth + 1);
+                        DFS(start * arr[depth], depth+1);
                         break;
                     case 3:
-                        DFS(number / map[depth], depth + 1);
+                        DFS(start / arr[depth], depth+1);
                         break;
                 }
                 operations[i]++;
             }
         }
     }
+
 }
