44d5e1809c7ac75836e75059ff24f73d14e8fa2b
KimJungWoo <86278689+tiktakz@users.noreply.github.com>
2024-01-18 17:13:27 +0900

    [Silver III] Title: N과 M (7), Time: 716 ms, Memory: 224000 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,46 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static int n,m;
+    static boolean[] check;
+    static int[] output;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        check = new boolean[n];
+        output = new int[m];
+
+        recur(0);
+        System.out.println(sb);
+        sc.close();
+    }
+
+    public static void recur(int depth) {
+        if (depth == m) {
+            for (int i=0; i<m; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i=0; i<n; i++) {
+            output[depth] = arr[i];
+            recur(depth + 1);
+        }
+    }
+}
